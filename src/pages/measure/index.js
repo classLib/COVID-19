627ec104f5577937483cadb53110ee4b74c18ce2
youngwind <75987901@qq.com>
2021-11-28 18:19:31 +0800

    feat: 更新资源

--- src/pages/measure/index.js
+++ src/pages/measure/index.js
@@ -1,5 +1,10 @@
 /**
  * 措施JS入口
  */
-import "bootstrap";
-import "bootstrap/dist/css/bootstrap.css";
+ import "bootstrap";
+ import "bootstrap/dist/css/bootstrap.css";
+ import "./index.css";
+ 
+ import { select } from "d3-selection";
+ 
+ select("body").append("h1").text("Hello wolrd!").attr('class','hello-world');
--- src/pages/vaccinate/index.js
+++ src/pages/vaccinate/index.js
@@ -1,5 +1,10 @@
 /**
  * 疫苗JS入口
  */
-import "bootstrap";
-import "bootstrap/dist/css/bootstrap.css";
+ import "bootstrap";
+ import "bootstrap/dist/css/bootstrap.css";
+ import "./index.css";
+ 
+ import { select } from "d3-selection";
+ 
+ select("body").append("h1").text("Hello wolrd!").attr('class','hello-world');

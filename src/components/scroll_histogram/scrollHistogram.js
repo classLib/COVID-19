d52d9f44b76084e14245eb81f25b6b754ecdad6c
628014 <965068089@qq.com>
2021-11-28 23:00:58 +0800

    。。。

---
+++ src/components/scroll_histogram/scrollHistogram.js
@@ -0,0 +1,8 @@
+export default class ScrollHistogram {
+
+  constructor(id, data) {
+    this.id = id;
+    this.data = data;
+  }
+
+}
--- src/pages/overview/index.js
+++ src/pages/overview/index.js
@@ -5,6 +5,52 @@ import "bootstrap";
 import "bootstrap/dist/css/bootstrap.css";
 import "./index.css";
 
-import { select } from "d3-selection";
+import {
+  select
+} from "d3-selection";
 
-select("body").append("h1").text("Hello wolrd!").attr('class','hello-world');
+import WorldMap from "../../components/map/worldMap";
+
+const create = async () => {
+
+  const data = await d3.csv('../../assets/data/WHO_COVID_19_global_data.csv').then((data) => {
+    d3.json('./data/worldGeo.json').then(geo => {
+      data.forEach(d => {
+        d['mortality'] = (+d['Cumulative_cases']) / (+d['Cumulative_cases']);
+      })
+      data.columns.push('mortality'); //添加死亡率
+
+      let groupedByCountryName = Array.from(d3.group(data, d => d.Country), ([key, value]) => ({
+        key,
+        value
+      }))
+      let groupedByCountryDate = Array.from(d3.group(data, d => d.Date_reported), ([key, value]) => ({
+        key,
+        value
+      }))
+
+      console.log(groupedByCountryDate)
+      console.log(groupedByCountryName)
+
+      // 4. 绘制柱状图
+      // let histogram = new MyHistogram('histogram', groupedByCountryName);
+
+      // 5. 绘制地图
+      // let worldMap = new WorldMap('worldMap', groupedByCountryDate, geo);
+      // worldMap.setHistogram(histogram);
+
+      // // 6. 绘制气泡图
+      // let bubble = new Bubble('bubble', groupedByCountryDate);
+      // bubble.setHistogram(histogram);
+      // bubble.setWorldMap(worldMap);
+
+    }).catch(error => {
+      console.log(error);
+    })
+  }).catch((error) => {
+    console.log(error);
+  })
+  console.log(data)
+
+}
+let start = create();
